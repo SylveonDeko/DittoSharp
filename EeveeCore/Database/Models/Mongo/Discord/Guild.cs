a591fdef4d72c38477e871d66afb081e0607fefe
SylveonDeko <crimsonslayer76@gmail.com>
2025-03-23 17:20:20 -0400

    maybe

--- EeveeCore/Database/Models/Mongo/Discord/Guild.cs
+++ EeveeCore/Database/Models/Mongo/Discord/Guild.cs
@@ -16,16 +16,13 @@ public class Guild
     [BsonElement("prefix")] public string Prefix { get; set; }
 
     [BsonElement("disabled_channels")]
-    [BsonSerializer(typeof(EmptyListSerializer<ulong>))]
-    public List<ulong> DisabledChannels { get; set; }
+    public List<ulong> DisabledChannels { get; set; } = [];
 
     [BsonElement("redirects")]
-    [BsonSerializer(typeof(EmptyListSerializer<ulong>))]
-    public List<ulong> Redirects { get; set; }
+    public List<ulong> Redirects { get; set; } = [];
 
     [BsonElement("disabled_spawn_channels")]
-    [BsonSerializer(typeof(EmptyListSerializer<ulong>))]
-    public List<ulong> DisabledSpawnChannels { get; set; }
+    public List<ulong> DisabledSpawnChannels { get; set; } = [];
 
     [BsonElement("pin_spawns")] public bool PinSpawns { get; set; }
 
@@ -38,8 +35,7 @@ public class Guild
     [BsonElement("modal_view")] public bool ModalView { get; set; }
 
     [BsonElement("enabled_channels")]
-    [BsonSerializer(typeof(EmptyListSerializer<ulong>))]
-    public List<ulong> EnabledChannels { get; set; }
+    public List<ulong> EnabledChannels { get; set; } = [];
 
     [BsonElement("enable_spawns_all")] public bool EnableSpawnsAll { get; set; }
 
